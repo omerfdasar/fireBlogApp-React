ee519e96e25411dfc5127f91af6ca434759e6756
Omer <omerfarukdasar@gmail.com>
2022-04-22 19:30:47 +0200

    Router Created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import AppRouter from "./app-router/AppRouter";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-    
+      <AppRouter />
     </div>
   );
 }
---
+++ src/app-router/AppRouter.js
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import About from "../pages/About";
+import Dashboard from "../pages/Dashboard";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+
+const AppRouter = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        {/* <Route path="/" element={<Dashboard />} /> */}
+        <Route path="/about/" element={<About />} />
+        <Route path="/" element={<Login />} />
+        {/* <Route path="/" element={<Register />} /> */}
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRouter;
