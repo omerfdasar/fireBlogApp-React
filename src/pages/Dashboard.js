522ea5316d421bd127e89b4b1e8c8e759043abf0
Omer <omerfarukdasar@gmail.com>
2022-04-29 17:20:38 +0200

    updated

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -17,30 +17,30 @@ const Dashboard = () => {
   // console.log(blogsDash);
   const theme = createTheme();
 
-  const callData = () => {
-    const [isLoading, setIsLoading] = useState();
-    const [blogsDash, setBlogsDash] = useState();
-    useEffect(() => {
-      setIsLoading(true);
-      const db = getDatabase();
-      const blogRef = ref(db, "blog");
+  const [isLoading, setIsLoading] = useState();
+  const [blogsDash, setBlogsDash] = useState();
 
-      onValue(blogRef, (snapshot) => {
-        const data = snapshot.val();
-        const dataList = [];
+  useEffect(() => {
+    setIsLoading(true);
+    const db = getDatabase();
+    const blogRef = ref(db, "blog");
 
-        // i can also use shallow copy as using spread
-        for (let id in data) {
-          dataList.push({ id, ...data[id] });
-        }
-        setBlogsDash(dataList);
-        setIsLoading(false);
-      });
-    }, []);
-    return { isLoading, blogsDash };
-  };
-  
-  const { isLoading, blogsDash } = callData();
+    onValue(blogRef, (snapshot) => {
+      const data = snapshot.val();
+      const dataList = [];
+
+      // i can also use shallow copy as using spread
+      for (let id in data) {
+        dataList.push({ id, ...data[id] });
+      }
+      setBlogsDash(dataList);
+      setIsLoading(false);
+    });
+  }, []);
+
+  // return { isLoading, blogsDash };
+
+  // const { isLoading, blogsDash } = callData();
   return (
     <div>
       <Typography sx={{ color: "#046582", textAlign: "center", fontSize: 40 }}>
